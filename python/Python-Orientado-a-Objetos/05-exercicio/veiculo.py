3ed4c8e6c44530b2b6f413005dd6b90c87e9eedd
T-Riego <tiagobboy@hotmail.com>
2025-10-23 17:14:01 -0300

    Organizando Projetos por materia

--- python/Python-Orientado-a-Objetos/05-exercicio/veiculo.py
+++ python/Python-Orientado-a-Objetos/05-exercicio/veiculo.py
@@ -8,6 +8,14 @@ class Veiculo(ABC):
     @abstractmethod
     def frear(self):
         pass
+    
+    @abstractmethod
+    def ligar(self):
+        pass
+    
+    def voar(self):
+        return "O veículo está voando."
+
 
 class Carro(Veiculo):
     def mover(self):
@@ -16,19 +24,39 @@ class Carro(Veiculo):
     def frear(self):
         return "O carro está freando."
     
+    def ligar(self):
+        return "O carro está ligando."
+
 class Bicicleta(Veiculo):
     def mover(self):
         return "A bicicleta está se movendo."
     
     def frear(self):
         return "A bicicleta está freando."
+    
+    def ligar(self):
+        return "A bicicleta não possui ignição."
   
+class Aviao(Veiculo):
+    def mover(self):
+        return "O avião está voando."
+    
+    def frear(self):
+        return "O avião está aterrissando."
+
+    def ligar(self):
+        return "O avião está ligando."
+
   # Criando objetos para teste
 carro = Carro()
 bicicleta = Bicicleta()
+aviao = Aviao()
 
 print(carro.mover()) 
 print(carro.frear())
 
 print(bicicleta.mover())
 print(bicicleta.frear())
+
+print(aviao.mover())
+print(aviao.ligar())
